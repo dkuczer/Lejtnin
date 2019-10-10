6d33ff26225f6f2633942da80328068cc926ba58
dkuczer <dawid.kuczer@britenet.com.pl>
2019-10-10 23:57:55 +0200

    search item click handler now navigates to product page with product id as parameter

--- src/aura/LTS_CustomSearchResultItem/LTS_CustomSearchResultItemController.js
+++ src/aura/LTS_CustomSearchResultItem/LTS_CustomSearchResultItemController.js
@@ -1,5 +1,8 @@
 ({
     handleItemClick: function (component, event, helper) {
         console.log(component.get('v.recordId'));
+        var navEvt = $A.get('e.force:navigateToURL');
+        navEvt.setParams({url: '/lts-product-page?productId=' + component.get('v.recordId')});
+        navEvt.fire();
     }
 })
