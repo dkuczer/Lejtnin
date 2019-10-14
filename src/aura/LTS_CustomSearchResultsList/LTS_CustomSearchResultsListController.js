f92715633b698f36f64083a21a841469b65464e7
dkuczer <dawid.kuczer@britenet.com.pl>
2019-10-14 10:25:04 +0200

    Categories, css and minor changes

--- src/aura/LTS_CustomSearchResultsList/LTS_CustomSearchResultsListController.js
+++ src/aura/LTS_CustomSearchResultsList/LTS_CustomSearchResultsListController.js
@@ -6,5 +6,32 @@
             component.set('v.recordIds', ids);
             sessionStorage.removeItem('customSearch--recordIds');
         }
+
+        var action = component.get('c.getProductFamilies');
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                var families = response.getReturnValue();
+                component.set('v.productFamilies', families)
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    searchByFamily: function (component, event, helper) {
+        var families = component.get('v.productFamilies');
+        var familyIndex = event.target.id;
+        var action = component.get('c.searchForIdsByFamily');
+        action.setParams({familyString: families[familyIndex]});
+
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                var ids = response.getReturnValue();
+                component.set('v.recordIds', ids)
+            }
+        });
+
+        $A.enqueueAction(action);
     }
 })
