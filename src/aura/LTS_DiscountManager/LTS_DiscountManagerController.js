97fc5dfc86b4f88df9ae1d814a0e3fcbe1dc2a8f
dkuczer <dawid.kuczer@britenet.com.pl>
2019-10-18 15:10:37 +0200

    Product searching

--- src/aura/LTS_DiscountManager/LTS_DiscountManagerController.js
+++ src/aura/LTS_DiscountManager/LTS_DiscountManagerController.js
@@ -1,3 +1,12 @@
 ({
-
+    doInit: function(component, event, helper){
+        helper.handleInit(component);
+        helper.getProducts(component);
+    },
+    getProducts: function (component, event, helper) {
+        helper.getProducts(component);
+    },
+    selectProduct: function (component, event, helper) {
+        helper.selectProduct(component, event);
+    }
 })
--- src/aura/LTS_DiscountManager/LTS_DiscountManagerHelper.js
+++ src/aura/LTS_DiscountManager/LTS_DiscountManagerHelper.js
@@ -1,3 +1,52 @@
 ({
+    handleInit: function(component){
+        var action = component.get('c.getProductsFamilies');
+        action.setCallback(this, function(response){
+            if(response.getState() === 'SUCCESS'){
+                var results = response.getReturnValue();
+                component.set('v.productsFamilies', results);
+            }
+            else{
+                console.log('Hey, at least js is working');
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    getProducts: function (component) {
 
+        var productWrappers = component.get('v.availableProducts');
+        var productFamily = component.find('selectFamily').get('v.value');
+        var productName = component.get('v.productName');
+        var prodsId = [];
+
+        productName = (productName == null? '': productName);
+
+        var action = component.get('c.searchForProducts');
+        action.setParams({
+            prodName: productName,
+            prodFamily: productFamily,
+            chosenProdsIds: prodsId
+        });
+        action.setCallback(this, function(response){
+            if(response.getState() === 'SUCCESS'){
+                var results = response.getReturnValue();
+                component.set('v.availableProducts', results);
+                console.log('results');
+            }
+            else{
+                console.log('Hey, at least js is working');
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    selectProduct: function (component, event) {
+        var availableProducts = component.get('v.availableProducts');
+        var selectedProdIndex = event.target.id;
+        var selectedProducts = component.get('v.selectedProducts');
+
+        // selectedProducts.push(availableProducts[selectedProdIndex]);
+        // availableProducts.slice(selectedProdIndex, 1);
+
+        // component.set('v.selectedProducts', selectedProducts);
+    }
 })
